d337f5315a2ced7210aa316bb3895797569d63a6
Luis <luisfernandogvv@gmail.com>
2023-03-01 10:56:15 -0300

    refactor: ...

--- src/events/whatsapp-events.ts
+++ src/events/whatsapp-events.ts
@@ -34,7 +34,7 @@ export const whatsEvents = {
 
     },
 
-    onMessage (message: Message, owner: IlordOwnerProps, stateManager: any){
+    onMessage (message: Message, owner: IlordOwnerProps, stateManager: () => any){
 
         const { from: number, body } = message;
 
