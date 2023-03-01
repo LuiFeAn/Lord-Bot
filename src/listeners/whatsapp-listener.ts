37aeb63de7b8604efc005988deb55e8635c366cb
Luis <luisfernandogvv@gmail.com>
2023-03-01 09:40:54 -0300

    feat: Modularizando listeners do WhatsApp

---
+++ src/listeners/whatsapp-listener.ts
@@ -0,0 +1,18 @@
+import { IlordOwnerProps } from "../interfaces/lord-bot";
+
+import { whatsProvider } from "../providers/whatsapp-provider";
+import { whatsEvents } from "../events/whatsapp-events";
+
+export default function whatsListener(owner: IlordOwnerProps, stateManager: any){
+
+    whatsProvider.on('qr', whatsEvents.onQrCode);
+
+    whatsProvider.on('authenticated', whatsEvents.onAuth);
+
+    whatsProvider.on('auth_failure', whatsEvents.onFail);
+
+    whatsProvider.on('ready', whatsEvents.onReady);
+
+    whatsProvider.on('message', message => whatsEvents.onMessage( message, owner, stateManager ));
+
+}
