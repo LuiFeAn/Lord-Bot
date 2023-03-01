91b600454c79a534feef91bc59cd925a885f0b19
Luis <luisfernandogvv@gmail.com>
2023-03-01 09:40:39 -0300

    feat: Aplicando função modular referente ao Listener do whatsapp

--- src/lib/bot.ts
+++ src/lib/bot.ts
@@ -1,10 +1,10 @@
-import qrcode from 'qrcode-terminal';
-
 import { whatsProvider } from "../providers/whatsapp-provider";
 
-import { ClientSession, Message } from 'whatsapp-web.js';
-
 import { ILordBot, IlordBotStates } from '../interfaces/lord-bot';
+
+import { whatsEvents } from '../events/whatsapp-events';
+
+import whatsListener from "../listeners/whatsapp-listener";
 
 class LordBot {
 
@@ -24,56 +24,7 @@ class LordBot {
 
         whatsProvider.initialize();
 
-        function onReady(){
-
-            console.log('O LordBOT está preparado para uso');
-    
-        }
-    
-        function onQrCode(code: string){
-    
-            console.log('Por favor, utilize o QrCODE abaixo para vincular seu número ao LordBOT')
-    
-            qrcode.generate(code,{
-                small:true
-            });
-    
-        }
-    
-        function onAuth(client: ClientSession){
-            
-            console.log('LordBot foi autenticado com sucesso');
-    
-        }
-    
-        function onFail(message: string){
-    
-            console.log('Um erro ocorreu durante o vinculo do LordBOT ao sue número');
-    
-        }
-    
-        const onMessage = (message: Message) => {
-    
-            const { from: number, body } = message;
-
-            if( number === this.owner.number ){
-
-                this.stateVerification();
-
-            }
-            
-    
-        }
-
-        whatsProvider.on('qr',onQrCode);
-
-        whatsProvider.on('authenticated',onAuth);
-
-        whatsProvider.on('auth_failure',onFail);
-
-        whatsProvider.on('ready',onReady);
-
-        whatsProvider.on('message',onMessage);
+        whatsListener(this.owner,this.stateManager);
 
     }
 
@@ -83,7 +34,7 @@ class LordBot {
 
     }
 
-    stateVerification(){
+    stateManager(){
 
         this.states.forEach( state => {
 
@@ -91,15 +42,11 @@ class LordBot {
 
             if( this.owner.state === name ){
 
-                execute();
+                execute(this.owner.message!);
 
             }
 
         })
-
-    }
-
-    messageRecivied(){
 
     }
 
