5c6aa2143309bd393c1d2b25ee467390606f3a2e
Luis <luisfernandogvv@gmail.com>
2023-04-19 12:09:55 -0300

    refactor: Removendo a interface "ILordBot"

--- src/interfaces/lord-bot.ts
+++ src/interfaces/lord-bot.ts
@@ -1,26 +1,4 @@
 import whatsapp from 'whatsapp-web.js';
-import UsersManager from '../lib/users-manager.js';
-import { ChatMessage } from 'chatgpt';
-
-export interface ILordBot {
-
-    readonly name: string;
-
-    owner: ILordOwnerConstuctor;
-
-    userManager: UsersManager;
-
-    gptRequest: boolean;
-
-    initialize(): Promise<void>;
-
-    sendMessage(number: string, message: string, options?: { withAudio: boolean }): Promise<void>;
-
-    onState(states: IlordBotStates[]): void;
-
-    gpt(message: string, { type }: IGpt): Promise<string | ChatMessage>;
-
-}
 
 export interface ILordBotConstructor {
 
@@ -59,7 +37,7 @@ export interface ILordOwnerConstuctor  {
 export interface IGpt {
 
     /**
-           Defines which type of API you would like to use. The "Official" uses the official OpenAI server, the "unofficial" uses a third-party proxy
+         Defines which type of API you would like to use. The "Official" uses the official OpenAI server, the "unofficial" uses a third-party proxy
       */
     type: 'official' | 'unofficial'
 
